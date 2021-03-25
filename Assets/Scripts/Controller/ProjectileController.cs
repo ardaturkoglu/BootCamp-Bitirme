61659a451718a44a40e238a294d3b1b02b7bd255
Ugur Polat <ugurpolat113@gmail.com>
2021-03-25 22:46:19 +0300

    Initial commit.
    Added joystick controller.
    Added player animation and gun.

---
+++ Assets/Scripts/Controller/ProjectileController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileController : MonoBehaviour
+{
+    public Transform spawnProjectilePosition;
+    public Transform turretPivot;
+    public float projectileSpeed;
+    public GameObject projectilePrefab;
+    public float fireRate = 0f;
+    private float fireCountDown = 0f;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (Time.time >= fireCountDown)
+            {
+                FireProjectile();
+                fireCountDown = Time.time + 1 / fireRate;
+            }
+        }
+    }
+
+    void FireProjectile()
+    {
+        GameObject cloneProjectile = Instantiate(projectilePrefab, spawnProjectilePosition.position, turretPivot.transform.rotation);
+        Destroy(cloneProjectile,3);
+    }
+}
---
+++ Assets/Scripts/ProjectileInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileInfo : MonoBehaviour
+{
+    [SerializeField]
+    private float projectileSpeed = 0f;
+    void Update()
+    {
+        transform.Translate(Vector3.up * Time.deltaTime * projectileSpeed, Space.Self);
+    }
+}
